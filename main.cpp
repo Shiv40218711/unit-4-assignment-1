51ffc1bb2a487cab4fb36472a65b11f4cdf2c5f3
Shiv40218711 <40218711@hruc.ac.uk>
2025-04-30 15:20:52 +0100

    import from replit

--- main.cpp
+++ main.cpp
@@ -1,29 +1,33 @@
 #include<iostream> 
 using namespace std;
-//Function Prototypes 
 
+//Function Prototypes 
 void studentLogin();
 void EveOrglogin();
 
+// Function: welcome
+// Purpose: Display welcome message and direct users to appropriate login
 void welcome()
 {
-	cout << "Welcome to Student Affairs Departement" << endl;
-	cout << "This software is created for event organisers at HRUC" << endl;
-	cout << "Are you a student or organiser? [Enter in lowercase]" << endl;
-	string choice; //Creating a variable to store user's choice: student/staff
-	cin >> choice;
-	if (choice == "student") {//student login choice
-		studentLogin();
-	}
-	else if (choice == "organiser") {//staff login choice
-		EveOrglogin();
-	}
-	else {
-		cout << "Wrong Choice!" << endl;
-	}
+    cout << "Welcome to Student Affairs Departement" << endl;
+    cout << "This software is created for event organisers at HRUC" << endl;
+    cout << "Are you a student or organiser? [Enter in lowercase]" << endl;
+    string choice; //Creating a variable to store user's choice: student/staff
+    cin >> choice;
+    if (choice == "student") { //student login choice
+        studentLogin();
+    }
+    else if (choice == "organiser") { //staff login choice
+        EveOrglogin();
+    }
+    else {
+        cout << "Wrong Choice!" << endl;
+    }
 }
 
+// Function: main
+// Purpose: Program entry point
 int main() {
-	welcome(); //Calling the welcome function 
-	return 0;
+    welcome(); //Calling the welcome function 
+    return 0;
 }
